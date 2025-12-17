5055ee7647df996290b73d5bb3f301bf68669d2e
rabute231 <antoinerabute@gmail.com>
2025-12-17 14:51:22 +0100

    first two methods

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -1,11 +1,40 @@
+import { db } from "../db/database"
+import { collection } from "../db/schema"
+
 export const createCollection = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try{
+        console.log("Creating collection...")
+
+        const {title, description, isPublic} = request.body
+        const owner = request.user.userId
+
+        const newCollection = await db.insert(collection).values({title,description, public: isPublic, userId:owner}).returning()
+
+        response.status(201).send({message: "Collection created !"})
+    }catch(error){
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to create collection"
+        })
+    }
 }
 
 export const getCollection = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try{
+        console.log("Retrieving collection...")
+
+        const id = request.params
+
+        const collec = await db.select().from(collection).where(eq(collection.id, id)).returning()
+
+        response.send(200).json(collec)
+    }catch(error){
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to retrieve collection"
+        })
+    }
+    
 }
 
 export const getMyCollections = async (request, response) => {
