b95e6a58f57fb83fb4c6678b0a8db0be09d8cc4a
Lucas Demaimay <demaima231@unicaen.fr>
2025-12-18 08:52:56 +0100

    Test get current user

--- src/models/auth.js
+++ src/models/auth.js
@@ -9,5 +9,5 @@ export const registerSchema = z.object({
 
 export const loginSchema = z.object({
     email: z.email(),
-    password: z.string().min(8, 'Password must be at least 8 characters long').max(255, 'Password can not exceed 255 characters')
+    password: z.string()
 })
--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -2,11 +2,12 @@ import {Router} from 'express'
 import { getCurrentUser, login, register } from '../controllers/authController.js'
 import { validateBody } from '../middlewares/validation.js'
 import { loginSchema, registerSchema } from '../models/auth.js'
+import { authenticateToken } from '../middlewares/autenticateToken.js'
 
 const router = Router()
 
 router.post('/register', validateBody(registerSchema) ,register)
 router.post('/login', validateBody(loginSchema) ,login)
-router.get('/', getCurrentUser)
+router.get('/', authenticateToken, getCurrentUser)
 
 export default router
