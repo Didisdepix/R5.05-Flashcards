68dd48f2b110cd4787ff53558f40157c1fa1bb55
lucas_demaimay <demaimay.lucas@gmail.com>
2025-12-18 01:36:54 +0100

    Creation of flashcard

--- src/controllers/flashcardController.js
+++ src/controllers/flashcardController.js
@@ -1,34 +1,95 @@
+import { eq } from "drizzle-orm"
+import { db } from "../db/database.js"
+import { collection, flashcard } from "../db/schema.js"
+
 export const createFlashcard = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        const body = request.body
+        
+        const [targetCollection] = await db.select().from(collection).where(eq(collection.id, body.collectionId))
+        
+        if( targetCollection.userId != request.user.userId ) {
+            response.status(403).json({
+                error: "Unauthorized to create flashcard...",
+            })
+        }
+
+        const [createdFlashcard] = await db.insert(flashcard).values(body).returning()
+
+        response.status(201).json({
+            message: "Flashcard created successfully.",
+            data: createdFlashcard
+        })
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to create flashcard..."
+        })
+    }
 }
 
 export const getFlashcard = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to get flashcard..."
+        })
+    }
 }
 
 export const getFlashcards = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to get flashcards..."
+        })
+    }
 }
 
 export const getFlashcardsToLearn = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to get flashcards to learn..."
+        })
+    }
 }
 
 export const modifyFlashcard = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to modify flashcard..."
+        })
+    }
 }
 
 export const deleteFlashcard = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to delete flashcard..."
+        })
+    }
 }
 
 export const learnFlashcard = async (request, response) => {
-    console.log("isOk")
-    response.send()
+    try {
+        
+    } catch (error) {
+        console.error(error)
+        response.status(500).json({
+            error: "Failed to learn flashcard..."
+        })
+    }
 }
